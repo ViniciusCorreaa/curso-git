fc68c500cf6fc8ec6a15ce2a739da71c3d7e4bb3
Vinicius Oliveira <vinicius_correa@outlook.com>
2021-08-27 10:28:11 -0300

    Projeto iniciado, mostrando hello world

---
+++ ConfGitGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfGitGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
